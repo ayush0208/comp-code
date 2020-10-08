baf0cf55f752691bdeff6c0198d81d2eb2d42811
Ayush Garg <gargag1999@gmail.com>
2020-10-08 13:51:48 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,17 @@
+// Problem Link --> https://leetcode.com/problems/palindrome-number/
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        
+        int reverse =0;
+        int num =x;
+        while(num>0)
+        {
+            reverse=reverse*10+ num%10;
+            num/=10;
+        }
+        
+        return reverse==x;
+    }
+}
